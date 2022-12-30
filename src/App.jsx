62f0a9e905ac3d6be06fd8a1d5574c17e1f93f73
Avin Lanson Tharakan <avinlanson@gmail.com>
2022-12-29 19:12:04 -0500

    intitialzed project with components and index.css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header/header.jsx";
+import About from "./components/about/about.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <About />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/about/about.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./about.css";
+
+const about = () => {
+  return <div>Hi there!</div>;
+};
+
+export default about;
